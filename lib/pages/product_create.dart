d619e2b7323a01dd87ac57a2e6b7ca662da35965
Stephen Parks <parks.stephen@gmail.com>
2018-11-30 21:12:28 -0600

    improved user inputs and routed to products page

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -53,14 +53,21 @@ class _ProductCreatePageState extends State<ProductCreatePage>{
             priceValue = double.parse(value);
           });
         }),
-        RaisedButton(child: Text('Save'), onPressed: (){
+        SizedBox(
+          height:10.0
+        ),
+        RaisedButton(child: Text('Save'), 
+        color: Theme.of(context).accentColor,
+        textColor: Colors.white,
+        onPressed: (){
           final Map<String, dynamic> product ={
             'title': titleValue,
             'description': descriptionValue,
             'price': priceValue,
-            'image': 'assets/food.jpg'
+            'image': 'assets/food.jpg',
           };
           widget.addProduct(product);
+           Navigator.pushReplacementNamed(context, '/');
         })
         ],
       )
