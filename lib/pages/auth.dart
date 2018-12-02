311d400c430a3cae8d45966376863402cb7a79dc
Stephen Parks <parks.stephen@gmail.com>
2018-12-02 15:37:58 -0600

    auth page styling

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -22,51 +22,63 @@ class _AuthPageState extends State<AuthPage> {
           title: Text('Login'),
         ),
         body: Container(
-          decoration: BoxDecoration(
-              image:
-                  DecorationImage(image: AssetImage('.assets/background.jpg'),
-                  ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.5), BlendMode.dstATop),
+                image: AssetImage('assets/background.jpg'),
+              ),
+            ),
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+                child: SingleChildScrollView(
+              child: Column(children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'E-Mail',
+                      filled: true,
+                      fillColor: Colors.white),
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (String value) {
+                    setState(() {
+                      _emailValue = value;
+                    });
+                  },
                 ),
-          margin: EdgeInsets.all(10.0),
-          child: ListView(children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'E-Mail'),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (String value) {
-                setState(() {
-                  _emailValue = value;
-                });
-              },
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              onChanged: (String value) {
-                setState(() {
-                  _passwordValue = value;
-                });
-              },
-            ),
-            SwitchListTile(
-                value: _acceptTerms,
-                onChanged: (bool value) {
-                  setState(() {
-                    _acceptTerms = value;
-                  });
-                },
-                title: Text('Accept Terms')),
-            SizedBox(height: 10.0),
-            RaisedButton(
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              child: Text('Login'),
-              onPressed: () {
-                print(_emailValue);
-                print(_passwordValue);
-                Navigator.pushReplacementNamed(context, '/products');
-              },
-            )
-          ]),
-        ));
+                SizedBox(height: 10.0),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Password',
+                      filled: true,
+                      fillColor: (Colors.white)),
+                  obscureText: true,
+                  onChanged: (String value) {
+                    setState(() {
+                      _passwordValue = value;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                    value: _acceptTerms,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _acceptTerms = value;
+                      });
+                    },
+                    title: Text('Accept Terms')),
+                SizedBox(height: 10.0),
+                RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  textColor: Colors.white,
+                  child: Text('Login'),
+                  onPressed: () {
+                    print(_emailValue);
+                    print(_passwordValue);
+                    Navigator.pushReplacementNamed(context, '/products');
+                  },
+                )
+              ]),
+            ))));
   }
 }
