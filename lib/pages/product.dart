b785b74b3c3968539f9cf8154f98aa4fd6909ab8
Stephen Parks <parks.stephen@gmail.com>
2018-12-02 23:00:27 -0600

    similar to example

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,57 +8,65 @@ class ProductPage extends StatelessWidget {
   final String description;
   final double price;
   final String location;
- 
 
-  ProductPage(this.title, this.imageUrl, this.description, this.price, this.location);
+  ProductPage(
+      this.title, this.imageUrl, this.description, this.price, this.location);
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        onWillPop: () {
-          print('back button pressed');
-          Navigator.pop(context, false);
-          return Future.value(false);
-        },
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text(title),              
+      onWillPop: () {
+        print('back button pressed');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
+            Widget>[
+          Image.asset(imageUrl),
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Oswald',
+                ),
+              )),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Text('Location: ',
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontFamily: 'Oswald',
+                    fontWeight: FontWeight.bold)),
+            Text(location,
+                style: TextStyle(color: Colors.grey, fontFamily: 'Oswald')),
+            Container(
+              padding: EdgeInsets.all(5.0),
+              child: Text(
+                '|',
+                style: TextStyle(color: Colors.grey, fontFamily: 'Oswald'),
+              ),
             ),
-            body: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset(imageUrl),
-                  Container(padding: EdgeInsets.all(10.0), 
-                  child: Text(
-                    title, 
-                    style: TextStyle(
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Oswald',),)),
-                  Container(
-                      padding: EdgeInsets.all(10.0),
-                      child: Column(children: <Widget> [
-                      Text('Descrtiption: ' + description),
-                      SizedBox(height: 10.0,),
-                      Container(
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).accentColor,
-                          borderRadius: BorderRadius.circular(
-                            (5.0),
-                          )),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
-                      child: Text('Price: \$${price.toString()}', 
-                      style: TextStyle(
-                        color: Colors.white),),),
-                      SizedBox(height: 10.0),
-                      Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children:<Widget>[
-                      Text('Location: ', style: TextStyle(fontWeight: FontWeight.bold)),
-                      Text(location) ]
-                      ),
-                  ]))
-                ])));
+            Text(
+              'Price: \$${price.toString()}',
+              style: TextStyle(color: Colors.grey, fontFamily: 'Oswald'),
+            ),
+          ]),
+          SizedBox(height: 5.0),
+          Container(
+              margin: EdgeInsets.all(10.0),
+              child: Text(
+                description,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+              ))
+        ]),
+      ),
+    );
   }
 }
