eb8b0e850e127a7dfe71cecc442599c671c151d4
Stephen Parks <parks.stephen@gmail.com>
2018-12-02 23:03:56 -0600

    similar to example

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -15,58 +15,58 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () {
-        print('back button pressed');
-        Navigator.pop(context, false);
-        return Future.value(false);
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
-            Widget>[
-          Image.asset(imageUrl),
-          Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                title,
-                style: TextStyle(
-                  fontSize: 26.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Oswald',
-                ),
-              )),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            Text('Location: ',
-                style: TextStyle(
-                    color: Colors.grey,
-                    fontFamily: 'Oswald',
-                    fontWeight: FontWeight.bold)),
-            Text(location,
-                style: TextStyle(color: Colors.grey, fontFamily: 'Oswald')),
+        onWillPop: () {
+          print('back button pressed');
+          Navigator.pop(context, false);
+          return Future.value(false);
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
+                  Widget>[
+            Image.asset(imageUrl),
             Container(
-              padding: EdgeInsets.all(5.0),
-              child: Text(
-                '|',
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 26.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Oswald',
+                  ),
+                )),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Text('Location: ',
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'Oswald',
+                      fontWeight: FontWeight.bold)),
+              Text(location,
+                  style: TextStyle(color: Colors.grey, fontFamily: 'Oswald')),
+              Container(
+                padding: EdgeInsets.all(5.0),
+                child: Text(
+                  '|',
+                  style: TextStyle(color: Colors.grey, fontFamily: 'Oswald'),
+                ),
+              ),
+              Text(
+                'Price: \$${price.toString()}',
                 style: TextStyle(color: Colors.grey, fontFamily: 'Oswald'),
               ),
-            ),
-            Text(
-              'Price: \$${price.toString()}',
-              style: TextStyle(color: Colors.grey, fontFamily: 'Oswald'),
-            ),
+            ]),
+            SizedBox(height: 5.0),
+            Container(
+                margin: EdgeInsets.all(10.0),
+                child: Text(description,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Oswald',
+                    ))),
           ]),
-          SizedBox(height: 5.0),
-          Container(
-              margin: EdgeInsets.all(10.0),
-              child: Text(
-                description,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
-              ))
-        ]),
-      ),
-    );
+        ));
   }
 }
